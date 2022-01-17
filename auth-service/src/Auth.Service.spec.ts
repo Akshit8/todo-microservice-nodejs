fa0885af979d55b5baf895b0de05af5691fab8c1
Akshit8 <akshitsadana@gmail.com>
2022-01-17 19:10:34 +0530

    trying mocking typeorm

--- auth-service/src/Auth.Service.spec.ts
+++ auth-service/src/Auth.Service.spec.ts
@@ -1,5 +1,10 @@
 import { ServiceBroker } from "moleculer";
+import { mocked } from "ts-jest/utils";
+import { getCustomRepository } from "typeorm";
 import path from "path";
+import { User } from "./entity";
+
+jest.mock("typeorm");
 
 describe("TEST auth", () => {
   const broker = new ServiceBroker({ logger: false });
@@ -10,19 +15,37 @@ describe("TEST auth", () => {
     await broker.start();
   });
 
-  beforeEach(async () => {});
-
   afterAll(async () => {
     await broker.stop();
   });
 
   test("sample", async () => {
+    const user = new User();
+    user.id = 1;
+    user.username = "asd";
+    user.email = "asdsda";
+    user.password = "asdsd";
+    user.createdAt = new Date();
+    user.updatedAt = new Date();
+
+    const userRepo = {
+      saveNewUser: jest.fn().mockResolvedValueOnce(user)
+    };
+
+    // mocked(createConnection).mockReturnValueOnce();
+    mocked(getCustomRepository).mockReturnValueOnce(userRepo);
+
     const res: any = await broker.call("auth.signUp", {
       username: "asdsdf",
       email: "asdadasd@mail.com",
       password: "asdasds",
       confirmPassword: "asdasds"
     });
+
+    expect(getCustomRepository).toHaveBeenCalledTimes(1);
+    expect(getCustomRepository).toBeCalledWith(userRepo);
+
+    expect(userRepo.saveNewUser).toBeCalledTimes(1);
 
     expect(res).toBeDefined();
     expect(res.http_status_code).toEqual(201);
