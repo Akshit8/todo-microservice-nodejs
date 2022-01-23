a8b142773ab9602892421f4be826ec04c4d5dbeb
Akshit8 <akshitsadana@gmail.com>
2022-01-23 17:52:06 +0530

    dockerfile added for api gateway

--- api-gateway/src/api/middleware.ts
+++ api-gateway/src/api/middleware.ts
@@ -1,11 +1,35 @@
 import { NextFunction, Request, Response } from "express";
 
-export const TokenMiddleware = (
+export const tokenMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
   req.body.token = token;
+  next();
+};
+
+export const queryParamMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  for (const key in req.query) {
+    req.body[key] = req.query[key];
+  }
+  next();
+};
+
+export const urlParamsMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  for (const key in req.params) {
+    if (key === "id") {
+      req.body.id = req.params.id;
+    }
+  }
   next();
 };
--- api-gateway/src/api/router.ts
+++ api-gateway/src/api/router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { AuthControllerV1 } from "./auth.controller";
-import { TokenMiddleware } from "./middleware";
+import { tokenMiddleware } from "./middleware";
 import { TodoControllerV1 } from "./todo.controller";
 
 const router = Router();
 
-const authRouterV1 = AuthControllerV1.buildControllerRoutes(TokenMiddleware);
-const todoRouterV1 = TodoControllerV1.buildControllerRoutes(TokenMiddleware);
+const authRouterV1 = AuthControllerV1.buildControllerRoutes(tokenMiddleware);
+const todoRouterV1 = TodoControllerV1.buildControllerRoutes(tokenMiddleware);
 
 router.use("/v1/auth", authRouterV1);
 router.use("/v1/todo", todoRouterV1);
