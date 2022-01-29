78b29a75100d21d84f35189325bab740055e4c2d
Akshit8 <akshitsadana@gmail.com>
2022-01-29 20:22:32 +0530

    added single test for todo service

--- todo-service/src/utils/AuthToken.ts
+++ todo-service/src/utils/AuthToken.ts
@@ -1,4 +1,4 @@
-import { NotBeforeError, sign, TokenExpiredError, verify } from "jsonwebtoken";
+import { NotBeforeError, TokenExpiredError, verify } from "jsonwebtoken";
 import { AuthenticationError, AuthTokenError } from "../errors";
 
 export interface tokenPayload {
@@ -6,7 +6,6 @@ export interface tokenPayload {
 }
 
 export interface AuthToken {
-  signToken(payload: tokenPayload): Promise<string>;
   verifyToken(token: string): Promise<tokenPayload>;
 }
 
@@ -17,27 +16,6 @@ export class JWT implements AuthToken {
   constructor(secret: string, duration?: string) {
     this.secret = secret;
     this.duration = duration || "24h";
-  }
-
-  async signToken(payload: tokenPayload): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      sign(
-        payload,
-        this.secret,
-        { expiresIn: this.duration },
-        (err: Error | null, token: string | undefined): void => {
-          if (err) {
-            reject(new AuthTokenError(err.message));
-            return;
-          }
-          if (!token) {
-            reject(new AuthTokenError("auth token undefined"));
-            return;
-          }
-          resolve(token);
-        }
-      );
-    });
   }
 
   async verifyToken(token: string): Promise<tokenPayload> {
