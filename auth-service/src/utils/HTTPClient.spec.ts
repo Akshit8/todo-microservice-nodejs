b941cd74160fdb904de9098cbf4104faff019b17
Akshit8 <akshitsadana@gmail.com>
2022-03-07 14:27:57 +0530

    added test

--- auth-service/src/utils/HTTPClient.spec.ts
+++ auth-service/src/utils/HTTPClient.spec.ts
@@ -1,4 +1,17 @@
+import { AxiosRequestConfig } from "axios";
 import HTTPClient from "./HTTPClient";
+
+interface RequestInterface {
+  method: string;
+  url: string;
+  data?: any;
+  requestConfig?: AxiosRequestConfig;
+}
+
+interface ResponseInterface {
+  statusCode: number;
+  timestamp: Date;
+}
 
 describe("TEST HTTPClient", () => {
   let client: HTTPClient;
@@ -7,9 +20,141 @@ describe("TEST HTTPClient", () => {
     client = new HTTPClient();
   });
 
-  test("OK", () => {
+  test("OK", async () => {
     expect(client).toBeDefined();
+
+    const p: Promise<ResponseInterface>[] = [];
+
+    const requests: RequestInterface[] = [
+      {
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/todos/1"
+      },
+      {
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/todos/2"
+      },
+      {
+        method: "POST",
+        url: "https://jsonplaceholder.typicode.com/todos",
+        data: {
+          title: "foo",
+          completed: false,
+          userId: 1
+        },
+        requestConfig: {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      },
+      {
+        method: "POST",
+        url: "https://jsonplaceholder.typicode.com/todos",
+        data: {
+          title: "bar",
+          completed: false,
+          userId: 1
+        },
+        requestConfig: {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      }
+    ];
+
+    const expectedResponse: ResponseInterface[] = [
+      {
+        statusCode: 200,
+        timestamp: new Date()
+      },
+      {
+        statusCode: 200,
+        timestamp: new Date()
+      },
+      {
+        statusCode: 201,
+        timestamp: new Date()
+      },
+      {
+        statusCode: 201,
+        timestamp: new Date()
+      }
+    ];
+
+    requests.forEach((request) => {
+      if (request.method === "GET") {
+        p.push(client.get(request.url, request.requestConfig));
+      } else {
+        p.push(client.post(request.url, request.data, request.requestConfig));
+      }
+    });
+
+    const responses = await Promise.all(p);
+
+    for (let i = 0; i < responses.length; i++) {
+      expect(responses[i].statusCode).toEqual(expectedResponse[i].statusCode);
+    }
   });
 
-  test("ERROR", () => {});
+  test("ERROR", async () => {
+    expect(client).toBeDefined();
+
+    const p: Promise<ResponseInterface>[] = [];
+
+    const requests: RequestInterface[] = [
+      {
+        method: "GET",
+        url: "https://jsonplaceholde.typicode.com/todos/1"
+      },
+      {
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/todos/2234234"
+      },
+      {
+        method: "POST",
+        url: "https://jsonplaceholde.typicode.com/todos",
+        data: {
+          title: "foo",
+          completed: false,
+          userId: 1
+        },
+        requestConfig: {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      }
+    ];
+
+    const expectedResponse: ResponseInterface[] = [
+      {
+        statusCode: 822,
+        timestamp: new Date()
+      },
+      {
+        statusCode: 404,
+        timestamp: new Date()
+      },
+      {
+        statusCode: 822,
+        timestamp: new Date()
+      }
+    ];
+
+    requests.forEach((request) => {
+      if (request.method === "GET") {
+        p.push(client.get(request.url, request.requestConfig));
+      } else {
+        p.push(client.post(request.url, request.data, request.requestConfig));
+      }
+    });
+
+    const responses = await Promise.all(p);
+
+    for (let i = 0; i < responses.length; i++) {
+      expect(responses[i].statusCode).toEqual(expectedResponse[i].statusCode);
+    }
+  });
 });
--- auth-service/src/utils/HTTPClient.ts
+++ auth-service/src/utils/HTTPClient.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
 import Pqueue from "p-queue";
 
 const MAX_HTTP_CLIENT_RETRIES = 3;
+const HTTP_CLIENT_SLEEP_TIME = 1000;
+const HTTP_CLEINT_ERROR_CODE = 822;
 
 interface ClientOptions {
   requestTimeout: number;
@@ -34,22 +36,34 @@ class HTTPClient {
     let retriesCount = MAX_HTTP_CLIENT_RETRIES;
     let err: AxiosError | undefined;
 
+    const timestamp = new Date();
+
     while (retriesCount > 0) {
       this.logger.info(`Requesting ${config.url} retries left: ${retriesCount}`);
 
       try {
         const response = await this.axiosInstance.request(config);
 
-        this.logger.info(`Request ${config.url} succeeded`);
-        return response;
+        this.logger.info(
+          `Request ${config.url} succeeded with status code ${response.status}`
+        );
+
+        return {
+          timestamp,
+          statusCode: response.status
+        };
       } catch (e) {
         err = e as AxiosError;
 
         if (err.response) {
-          break;
+          if (err.response.status < 500) {
+            break;
+          }
         }
 
         retriesCount--;
+
+        await sleep(HTTP_CLIENT_SLEEP_TIME);
       }
     }
 
@@ -69,6 +83,11 @@ class HTTPClient {
         this.logger.error(`Request error: ${err.message}`);
       }
     }
+
+    return {
+      timestamp,
+      statusCode: err?.response?.status || HTTP_CLEINT_ERROR_CODE
+    };
   }
 
   async get(url: string, requestConfig?: AxiosRequestConfig) {
@@ -82,14 +101,6 @@ class HTTPClient {
   }
 }
 
-(async () => {
-  const cl = new HTTPClient();
-
-  // const response = await cl.get("https://jsonplaceholder.typicode.com/todos/1234");
-
-  const response = await cl.get("http://localhost:3000/todos/1234");
-
-  console.log(response);
-})();
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default HTTPClient;
