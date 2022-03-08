f44a67bf2fc0588d5f5968181275f7edf6ad3dfc
Akshit8 <akshitsadana@gmail.com>
2022-03-08 11:28:50 +0530

    fix: benchmark cleint script

--- auth-service/src/scripts/httpClient.ts
+++ auth-service/src/scripts/httpClient.ts
@@ -1,16 +1,24 @@
 import { HTTPClient } from "../utils";
 
 (async () => {
+  const start = Date.now();
+
   const p: Promise<{ timestamp: Date; statusCode: number }>[] = [];
 
   const client = new HTTPClient();
 
   p.push(client.get("https://jsonplaceholde.typicode.com/todos/1"));
   p.push(client.get("https://jsonplaceholde.typicode.com/todos/2"));
+  p.push(client.get("https://jsonplaceholder.typicode.com/todos/3"));
+  p.push(client.get("https://jsonplaceholder.typicode.com/todos/4"));
+  p.push(client.get("https://jsonplaceholder.typicode.com/todos/5"));
+  p.push(client.get("https://jsonplaceholder.typicode.com/todos/6"));
 
   const response = await Promise.all(p);
 
   response.forEach((res) => {
     console.log(res);
   });
+
+  console.log("Time taken:", (Date.now() - start) / 1000, "seconds");
 })();
